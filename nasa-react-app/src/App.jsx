446ede7a89661b08d44a9c383aaf7594c8df097f
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-25 14:32:58 +0300

    feat: integrate NASA API

--- nasa-react-app/src/App.jsx
+++ nasa-react-app/src/App.jsx
@@ -1,21 +1,38 @@
 import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
 import Main from "./components/Main";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false)
   const [showModal, setShowModal] = useState(false);
 
-  function handleToggleModal(){
-    setShowModal(!showModal)
+  function handleToggleModal() {
+    setShowModal(!showModal);
   }
+
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+      try {
+        const res = await fetch(url);
+        const apiDate = await res.json();
+        setData(apiDate);
+        console.log("DATA\n", apiDate);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
 
   return (
     <>
       <Main />
-      {showModal && (
-        <SideBar handleToggleModal={handleToggleModal}/>
-        )}
+      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
       <Footer handleToggleModal={handleToggleModal} />
     </>
   );
